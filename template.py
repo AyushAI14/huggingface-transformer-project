6de3e2d46c9f34e43fc2aeebf8f55e38778b70ac
AyushAI14 <ayushaiml14@gmail.com>
2025-05-29 17:26:13 +0530

    intiated the project made template file and installed all necessary files

--- template.py
+++ template.py
@@ -1,20 +1,41 @@
 import os 
 from pathlib import Path
+import logging
 
 file_dir = 'src'
 
  
 file_list = [
-    f'{file_dir}/__init__.py'
+    ".github/workflows/.gitkeep",
+    f"{file_dir}/__init__.py",
+    f"{file_dir}/components/__init__.py",
+    f"{file_dir}/utils/__init__.py",
+    f"{file_dir}/utils/common.py",
+    f"{file_dir}/logging/__init__.py",
+    f"{file_dir}/config/__init__.py",
+    f"{file_dir}/config/configuration.py",
+    f"{file_dir}/pipeline/__init__.py",
+    f"{file_dir}/entity/__init__.py",
+    f"{file_dir}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/research.ipynb"
+
 ]
 
-for filepath in file_list:
-    filepath = Path(filepath)
-    filedir,filename = os.path.split(filepath)
-    if filedir.strip() != " ":
-        os.makedirs(filedir,exist_ok='True')
-    if ( not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
-        with open(filepath,'w') as f:
-            pass
-    else:
-        logging.debug("Unable to create the files")
+try:
+    for filepath in file_list:
+        filepath = Path(filepath)
+        filedir , filename = os.path.split(filepath)
+        if filedir != '':
+            os.makedirs(filedir,exist_ok=True)
+        if (not os.path.exists(filepath) or (os.path.getsize==0)):
+            with open(filepath,'w') as f:
+                pass
+except Exception as e:
+    logging.DEBUG('Error occured while creating files',e)
